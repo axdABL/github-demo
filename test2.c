d72a13d59996daaf75773f7178bce11ab5b8d7dc
axdABL <abhijit.desale@acuitybrands.com>
2016-02-03 09:10:55 -0500

    Adding a third change to test2.c file.

--- test2.c
+++ test2.c
@@ -4,5 +4,6 @@ int main()
 {
 	printf("Hello World\n");
 	printf("This is a change made for a change to be committed/updated for the git to be pushed.\n");
+	printf("This is a new addition, third line/print statement to this code."\n);
 	return 0;
 }
